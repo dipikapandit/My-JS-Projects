6311f72096fcf48986f173472a7c2bcb0aedd9fa
Dipika Pandit <dipika.815421@pmc.tu.edu.np>
2025-09-26 21:31:03 +0545

    Refactor todolist to use objects with due dates

--- todolist.js
+++ todolist.js
@@ -1,5 +1,7 @@
-const todolist = ['Buy groceries', 'Walk the dog', 'Read a book']; 
- 
+const todolist = [{
+  name: 'Learn React',
+  dueDate: '2025-11-30'
+}]; 
 renderTodoList();
 
 
@@ -7,26 +9,40 @@ function renderTodoList() {
   
   let todoItems = '';
   for (let i = 0; i < todolist.length; i++) {
-    const todo = todolist[i];
-    const html = `<div>${todo}</div>`;
+    const todoObject = todolist[i];
+    // const name = todoObject.name; 
+    //const { name } = todoObject; 
+    //const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+
+    const html = `
+      <div>${name}</div> <div>${dueDate}</div>
+        <button onclick="
+          todolist.splice(${i}, 1);
+          renderTodoList();
+        " class = "delete-todo-button">Delete</button>
+      `;
     todoItems += html;
   }
-
   document.querySelector('.js-todo-items').innerHTML = todoItems;
 }
 
+
+
 function addTodo() {
-
   const inputElement = document.querySelector('.js-name-input');
-
   const name = inputElement.value;
 
-  todolist.push(name);
+  const dueDateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dueDateInputElement.value;
 
-  console.log(todolist);
+  todolist.push({ 
+    //name: name,
+    //dueDate: dueDate
+    name, dueDate }); //Object property shorthand
 
   inputElement.value = '';
+  dueDateInputElement.value = '';
 
   renderTodoList();
-
 }
